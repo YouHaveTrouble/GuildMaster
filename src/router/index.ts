f2c5304643ca3f69076ad9f8d0766da0ced297b4
youhavetrouble <youhavetrouble@youhavetrouble.me>
2023-09-16 15:26:36 +0200

    don't load paths dynamically to allow offline play

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,33 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
+import HomeView from '@/views/HomeView.vue';
+import QuestView from "@/views/QuestView.vue";
+import AdventurerView from "@/views/AdventurerView.vue";
+import TechnicalView from "@/views/TechnicalView.vue";
+
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'guild',
-      component: () => import('@/views/HomeView.vue')
+      component: HomeView,
     },
     {
       path: '/quests',
       name: 'quests',
-      component: () => import('@/views/QuestView.vue')
+      component: QuestView,
     },
     {
       path: '/adventurers',
       name: 'adventurers',
-      component: () => import('@/views/AdventurerView.vue')
+      component: AdventurerView,
     },
     {
       path: '/technical',
       name: 'technical',
-      component: () => import('@/views/TechnicalView.vue')
-    }
+      component: TechnicalView,
+    },
   ]
 })
 
