423444a13942f1d9f18965dbd91e8ec9aed64662
youhavetrouble <youhavetrouble@youhavetrouble.me>
2023-09-24 23:56:10 +0200

    configure vite plugin for pwa handling

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: 'script',
+      filename: 'service-worker.js',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,jpeg,svg,json}'],
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
